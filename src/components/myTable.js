acb6859448c417b1e41da265a855a6e4731b5ebf
oder-ua <oleksandr.derun@nure.ua>
2019-01-10 15:22:41 +0200

    table -> antd table

--- src/components/myTable.js
+++ src/components/myTable.js
@@ -1,42 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Table } from 'antd';
+
+
+const columns = [
+  {
+    key: 'USERNAME',
+    title: 'Username',
+    dataIndex: 'userName',
+    sorter: (a, b) => a.userName.length - b.userName.length,
+    sortDirections: ['descend','ascend'],
+  }, 
+  {
+    key: 'SURNAME',
+    title: 'Surname',
+    dataIndex: 'surName',
+    sorter: (a, b) => a.surName.length - b.surName.length,
+    sortDirections: ['descend','ascend'],
+  },
+  {
+    key: 'EMAIL',
+    title: 'Email',
+    dataIndex: 'email',
+    sorter: (a, b) => a.email.length - b.email.length,
+  },
+  {
+    key: 'RESPONSE',
+    title: 'Response',
+    dataIndex: 'response',
+  },
+]
+
+function onChange(pagination, filters, sorter) {
+  console.log('params', pagination, filters, sorter);
+}
 
 class myTable extends Component{
-  renderTableRow = () => {
-    return this.props.data.map(formData => {
-      return (
-        <React.Fragment key={formData.username + formData.surname + formData.email + formData.response}>
-          <tr>
-            <td>{formData.username}</td>
-            <td>{formData.surname}</td>
-            <td>{formData.email}</td>
-            <td>{formData.response}</td>
-          </tr>
-        </React.Fragment>
-      )
-    });
-  };
 
   render(){
     return(
       <React.Fragment>
-        <table style={{width: '60%', margin: '40px 20% 40px 20%'}}>
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Surname</th>
-              <th>Email</th>
-              <th>Response</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              (this.props.data.length > 0) ? 
-              (this.renderTableRow()) : 
-              (<React.Fragment></React.Fragment>)
-            }
-          </tbody>
-        </table>
+        <Table 
+          style={{width: '60%', margin: '40px 20% 40px 20%'}} 
+          columns={columns}
+          dataSource={this.props.data.map(
+            formdata => ({
+                rowkey: (formdata.username + formdata.surname + formdata.email + formdata.response),
+                userName: (formdata.username),
+                surName: formdata.surname,
+                email: formdata.email,
+                response: formdata.response,
+              })
+            )
+          }
+          rowKey={record => record.rowkey}
+          onChange={onChange}
+        />
       </React.Fragment>
     )
   }
